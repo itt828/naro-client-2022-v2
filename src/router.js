b3e9b2af78e9841412875cb998b97da8618be43a
itt828 <iittt828@gmail.com>
2022-05-22 21:20:24 +0900

    :zap: add NotFound page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
+import NotFound from "./pages/NotFound.vue";
 
-const routes = [{ path: "/", name: "home", component: HomePage }];
+const routes = [
+  { path: "/", name: "home", component: HomePage },
+  { path: "/:path(.*)", component: NotFound },
+];
 
 const router = createRouter({
   history: createWebHistory(),
