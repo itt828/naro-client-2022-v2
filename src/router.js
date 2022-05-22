1beca3f6271a8677686e5e2e24a5f97abd886a6f
itt828 <iittt828@gmail.com>
2022-05-22 22:01:37 +0900

    :sparkles: add citypage

--- src/router.js
+++ src/router.js
@@ -2,9 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import NotFound from "./pages/NotFound.vue";
 import AxiosPage from "./pages/AxiosPage.vue";
+import CityPage from "./pages/CityPage.vue";
+
 const routes = [
   { path: "/", name: "home", component: HomePage },
   { path: "/axios", name: "axios", component: AxiosPage },
+  { path: "/city/:cityName", name: "city", component: CityPage, props: true },
   { path: "/:path(.*)", component: NotFound },
 ];
 
