c196740aab8168e5f7a64039323dc941f3dde6a4
itt828 <iittt828@gmail.com>
2022-05-22 21:44:19 +0900

    :construction: add axiospage

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import NotFound from "./pages/NotFound.vue";
-
+import AxiosPage from "./pages/AxiosPage.vue";
 const routes = [
   { path: "/", name: "home", component: HomePage },
+  { path: "/axios", name: "axios", component: AxiosPage },
   { path: "/:path(.*)", component: NotFound },
 ];
 
