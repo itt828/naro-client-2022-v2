afeede2fff12c5d26f36841b642a42a2cd85257d
itt828 <iittt828@gmail.com>
2022-05-22 21:56:30 +0900

    :zap: fix axiospage

--- src/pages/AxiosPage.vue
+++ src/pages/AxiosPage.vue
@@ -1,3 +1,21 @@
+<script setup>
+import axios from "axios";
+const sendGet = () => axios.get("https://ent5o104rfsmh.x.pipedream.net");
+const sendPost = () =>
+  axios.post("https://ent5o104rfsmh.x.pipedream.net", {
+    traQID: "itt",
+    grade: 21,
+  });
+const sendPut = () =>
+  axios.put("https://ent5o104rfsmh.x.pipedream.net", {
+    testData: { string: "string", number: 1 },
+  });
+const sendDelete = () =>
+  axios.delete("https://ent5o104rfsmh.x.pipedream.net", {
+    data: { po: 1 },
+  });
+</script>
+
 <template>
   <div>
     <button @click="sendGet">GET</button>
